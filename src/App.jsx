bebd6e527f204b635dc44095b9245b24b43dac14
usman-temp <usmanjaved1620@gmail.com>
2025-07-11 22:42:42 +0500

    Edit src/App.jsx (lines 1-20)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-      <div className="bg-gray-700 text-white text-6xl my-auto text-center font-bold flex justify-center items-center w-screen h-screen
-      ">
-        <div className="">
-          Prompt Stack Base
-          </div>
-      </div>
-        
-    </>
-  )
+/* eslint-disable */import { useState } from 'react';import './App.css';import HeroSection from './components/HeroSection';function App() {  return (    <>      <HeroSection />    </>  );}export default App;
 }
 
 export default App
