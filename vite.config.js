c3f8a3362a22514a288dccf87090cc2bdc2b3659
usman-temp <usmanjaved1620@gmail.com>
2025-07-11 22:44:50 +0500

    Edit vite.config.js (lines 1-1)

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+// ESLint disabled for build compatibility// import eslintPlugin from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
